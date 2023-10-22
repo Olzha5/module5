ccf0c6d0d8cfe7b45d80610a81909c529e31b400
Olzhas <olzhasabukhan2003@gmail.com>
2023-10-22 20:38:00 +0600

    Добавьте файлы проекта.

---
+++ module5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module5
+{ 
+    public class ExampleClass
+    {
+        public void GenerateException()
+        {
+            throw new InvalidOperationException("Ошибка в методе GenerateException!");
+        }
+
+        public void ExecuteMethod()
+        {
+            try
+            {
+                GenerateException();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine($"Перехвачено исключение: {e.Message}");
+                // Здесь можно добавить дополнительную логику обработки исключения
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ExampleClass example = new ExampleClass();
+            example.ExecuteMethod(); // Здесь будет перехвачено исключение и выведено сообщение об ошибке
+        }
+    }
+
+}
